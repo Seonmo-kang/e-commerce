7d5faee43fe0256b4cf3de97ca009b611dc54330
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-03-17 22:22:05 -0400

    Created Account  model and Profile model

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,35 @@
+import email
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .forms import UserRegisterationForm, UserChangeForm
+from account.models import User,Profile
 
-# Register your models here.
+# from .forms import User, UserChangeForm
+
+# @admin.register(User, admin.site)
+class UserAdmin(BaseUserAdmin):
+    #The forms to add and change instances
+    form = UserChangeForm
+    add_form = UserRegisterationForm
+
+    list_display = ['id','email', 'password','is_staff','is_active','is_superuser','created_at']
+    list_display_links= ('email',)
+    list_filter = ('email',)
+
+    fieldsets = [
+        ('Infomation',
+        {'fields': ('email', 'password')})
+    ]
+
+    search_fields = ('email',)
+    ordering = ('-created_at',)
+    filter_horizontal = ()
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id','first_name','last_name','user']
+    list_filter = ('credit_method','state','zipcode')
+    search_fields = ('first_name',)
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Profile,ProfileAdmin)
