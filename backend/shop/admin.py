18cb931beb8cfc5f3c9985f0f7bfa4cafdd547b6
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-03-20 01:04:47 -0400

    Create Item, Category, SubCategory, Brand, Qty, Review, Carasel Admin

---
+++ backend/shop/admin.py
@@ -0,0 +1,39 @@
+from csv import list_dialects
+from re import search
+from django.contrib import admin
+from .models import Item,Qty,Category,SubCategory,Brand,Review, Carasel
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id','name','description','sell_price',
+    'unit_price','code','model','color','isAvailable']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name','slug']
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+
+class QtyAdmin(admin.ModelAdmin):
+    list_display = ['id','item','qty_on_hand','qty_on_purchase',
+                    'qty_in_transit','qty_on_sold','qty_in_cancel']
+    # fieldsets =[
+    #      ('Information',{'fields':('id','item')}),
+    #      ('Qty',{'fields':('qty_on_hand','qty_on_purchase',
+    #                  'qty_in_transit','qty_on_sold','qty_in_cancel')})
+    # ]
+    search_fields = ('item',)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'item', 'user', 'star', 'subject', 'context']
+
+class CaraselAdmin(admin.ModelAdmin):
+    list_display = ['alt','image']
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(SubCategory,SubCategoryAdmin)
+admin.site.register(Brand,BrandAdmin)
+admin.site.register(Qty,QtyAdmin)
+admin.site.register(Review,ReviewAdmin)
+admin.site.register(Carasel,CaraselAdmin)
