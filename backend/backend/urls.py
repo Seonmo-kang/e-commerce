e4a6c6258bcdf2a592ec9460acb84d084665923d
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-03-23 23:04:49 -0400

    Troubleshooting Not found static css file. solved : STATICFILES_DIR

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('account.urls')),
     path('shop/',include('shop.urls'))
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT) +static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
