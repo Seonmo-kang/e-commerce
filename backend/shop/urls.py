7a2d796de3e3e697da8a83fa2fe1a56707ad13cb
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-03-23 23:06:44 -0400

    Add other path for filtering.

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
-from .views import ShopView
+from django.urls import path, re_path, include
+from .views import ItemDetailView, ShopView, ItemFilterView
 urlpatterns = [
-    path('',ShopView.as_view(),name='shop home page')
+    path('',ShopView.as_view(),name='shopHomePage'),
+    path(r'item/<int:pk>',ItemDetailView.as_view(),name='ItemDetailPage'),
+    # itemlist filter using regex
+   re_path(r'^itemlist/(?P<category>[\w]*)/(?P<subcategory>[\w]*)/(?P<brand>[\w]*)/$'
+   ,ItemFilterView.as_view(),name='ItemFilterView'),
+   # itemlist filter using query
+#    path(r'itemlist/')
 ]
