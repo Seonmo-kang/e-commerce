61cf55bb3c12efaff7133504b46d759223e74123
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-03-24 20:16:05 -0400

    Create filter_list function for filtering items. import render_to_string

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,6 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.views import generic 
 
 from .models import Brand, Carasel, Category, Item, SubCategory
@@ -30,21 +33,49 @@ class ShopView(generic.ListView):
     def get_queryset(self):
         queryset = Item.objects.isAvailable()
         #filter
-        filters={}
-        print(self.request.GET) # Value inspection
-        # for loop in request.GET.items() = query parameters
-        for key, value in self.request.GET.items():
-            if key in ['category','subcategory','brand']:
-                print(len(self.request.GET.getlist(key))) # Value inspection
-                if(len(self.request.GET.getlist(key))>1):
-                    filters[key+"__name__in"]=self.request.GET.getlist(key)
-                else:
-                    filters[key+"__name"]=self.request.GET[key]
-        print(filters) # Value inspection
-        return queryset.filter(**filters)
+        # filters={}
+        # print(self.request.GET) # Value inspection
+        # # for loop in request.GET.items() = query parameters
+        # for key, value in self.request.GET.items():
+        #     if key in ['category[]','subcategory[]','brand[]']:
+        #         print(len(self.request.GET.getlist(key))) # Value inspection
+        #         if(len(self.request.GET.getlist(key))>1):
+        #             print("key is {0}",key[0:-2])
+        #             filters[key[0:-2]+"__name__in"]=self.request.GET.getlist(key)
+        #         else:
+        #             print("key is {0}",key[0:-2])
+        #             filters[key[0:-2]+"__name"]=self.request.GET[key]
+        # print(filters) # Value inspection
+        # r_string = render_to_string()
+        # {queryset.filter(**filters)}
+        # return queryset.filter(**filters)
+        return queryset
 
-
+def filter_list(request):
+    queryset = Item.objects.isAvailable()
+    #filter
+    filters={}
+    print(request.GET) # Value inspection
+    # for loop in request.GET.items() = query parameters
+    for key, value in request.GET.items():
+        if key in ['category[]','subcategory[]','brand[]']:
+            print(len(request.GET.getlist(key))) # Value inspection
+            if(len(request.GET.getlist(key))>1):
+                print("key is {0}",key[0:-2])
+                filters[key[0:-2]+"__name__in"]=request.GET.getlist(key)
+            else:
+                print("key is {0}",key[0:-2])
+                filters[key[0:-2]+"__name"]=request.GET[key]
+    print(filters) # Value inspection
     
+    filtered_queryset = queryset.filter(**filters).order_by('-created_at') # queryset
+    print("filtered list :",filtered_queryset)
+
+    filtered_list = render_to_string('shop/filtered_list.html',{'item_list': filtered_queryset})
+    print("r_string is ",filtered_list)
+    return JsonResponse({'data':filtered_list})
+
+
 class ItemDetailView(generic.DetailView):
     model = Item
     template_name = "shop/itemDetail.html"
@@ -53,32 +84,4 @@ class ItemDetailView(generic.DetailView):
     def get_object(self,queryset=None):
         return Item.objects.detail(id=self.kwargs['pk'])
 
-class ItemFilterView(ShopView):
-
-    def get_context_data(self, **kwargs):
-        context = super(ItemFilterView,self).get_context_data(**kwargs)
-        #test code
-        # context['category'] =category
-        # context['subcategory'] =subcategory
-        # context['brand'] =brand
-        print(self.kwargs)
-        for key,value in self.kwargs.items():
-            if(value==''):
-                self.kwargs.pop(key)
-            # print(key)
-            # print(value)
-        print(self.kwargs)
-        # print(brand)
-        # print(category)
-        # print(subcategory)
-
-        itemlist = Item.objects.itemList()
-        # category=category,subcategory=subcategory,brand=brand
-        images = Carasel.objects.all()
-
-        context['images'] = images
-        context['media_url'] = settings.MEDIA_URL
-        context['item'] = itemlist
-        context['text']= 'This is context[text] context value.'
-        return context
 
