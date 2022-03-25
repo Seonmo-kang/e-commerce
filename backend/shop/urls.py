0991d32f6670f41c30b7da05749911a04694d958
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-03-24 20:15:13 -0400

    Using filter_list  for filtering items in shop website

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path, include
-from .views import ItemDetailView, ShopView, ItemFilterView
+from .views import ItemDetailView, ShopView, filter_list
 urlpatterns = [
     path('',ShopView.as_view(),name='shopHomePage'),
+    path('filter-list/',filter_list,name='filter list'),
     path(r'item/<int:pk>',ItemDetailView.as_view(),name='ItemDetailPage'),
     # itemlist filter using regex
-   re_path(r'^itemlist/(?P<category>[\w]*)/(?P<subcategory>[\w]*)/(?P<brand>[\w]*)/$'
-   ,ItemFilterView.as_view(),name='ItemFilterView'),
+#    re_path(r'^itemlist/(?P<category>[\w]*)/(?P<subcategory>[\w]*)/(?P<brand>[\w]*)/$'
+#    ,ItemFilterView.as_view(),name='ItemFilterView'),
    # itemlist filter using query
 #    path(r'itemlist/')
 ]
