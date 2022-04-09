3a72287721ae277733d0d61f5c51d3ab165ac3eb
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-04-09 17:08:38 -0400

    Created Item objects for a test.

--- backend/useraccount/tests.py
+++ backend/useraccount/tests.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
 # Create your tests here.
 
-from shop.models import *
-Item.objects.create(name='Test_item2',
-                    description='This is test item2'
-                    ,sell_price=1,code='P12345689013',
-                    model='Q123410811',
-                    weight=12.12,
-                    made_in='kr',
-                    color='mix',
-                    brand=Brand.objects.get(id=1),
-                    category=Category.objects.get(id=1),
-                    subCategory=SubCategory.objects.get(id=1)
-                    )
+# from shop.models import *
+# Item.objects.create(name='Test_item2',
+#                     description='This is test item2'
+#                     ,sell_price=1,code='P12345689013',
+#                     model='Q123410811',
+#                     weight=12.12,
+#                     made_in='kr',
+#                     color='mix',
+#                     brand=Brand.objects.get(id=1),
+#                     category=Category.objects.get(id=1),
+#                     subCategory=SubCategory.objects.get(id=1)
+#                     )
