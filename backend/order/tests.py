cb533ea8d1e42b8bffb80b221254b5827c04212f
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-04-09 17:01:06 -0400

    Delete Session key from all uesrs. Used python django shell to execute this codes.

--- backend/order/tests.py
+++ backend/order/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from .models import Sess
 # Create your tests here.
+
+ from django.contrib.sessions.models import Session
+from django.contrib.sessions.models import Session
+ Session.objects.all().delete()
