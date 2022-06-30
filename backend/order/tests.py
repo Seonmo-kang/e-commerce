facc4fb8ecc5a115e51378dc111503856fbb7a17
Seonmo <tjsah18@gmail.com>
2022-06-30 00:35:59 -0400

    fix : arrange the code

--- backend/order/tests.py
+++ backend/order/tests.py
@@ -2,6 +2,5 @@ from django.test import TestCase
 from .models import Sess
 # Create your tests here.
 
- from django.contrib.sessions.models import Session
 from django.contrib.sessions.models import Session
- Session.objects.all().delete()
+Session.objects.all().delete()
